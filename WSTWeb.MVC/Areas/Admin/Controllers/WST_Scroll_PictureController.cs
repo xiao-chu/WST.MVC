48824c541286ad8b9f3e7bc2472b56070f790094
sxy-zzj <79304949+sxy-zzj@users.noreply.github.com>
2021-03-02 10:37:10 +0800

    宋喜燕栏目管理和滚动图片

---
+++ WSTWeb.MVC/Areas/Admin/Controllers/WST_Scroll_PictureController.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WSTWeb.MVC.Areas.Admin.Controllers
+{
+    public class WST_Scroll_PictureController : Controller
+    {
+        //轮播图
+        [Area("Admin")]
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+    }
+}
