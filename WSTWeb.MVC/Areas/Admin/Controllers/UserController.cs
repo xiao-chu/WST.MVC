4d61f1fe8814157b59a861c014c270d25c792389
修龙 储 <cxl13399642800@outlook.com>
2021-03-02 10:06:20 +0800

    首页、登录功能的完善

---
+++ WSTWeb.MVC/Areas/Admin/Controllers/UserController.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WSTWeb.MVC.Areas.Admin.Controllers
+{
+    public class UserController : Controller
+    {
+        [Area("Admin")]
+        //登录
+        public IActionResult Login()
+        {
+            return View();
+        }
+        //编辑个人信息
+        [Area("Admin")]
+        public IActionResult Edit()
+        {
+            return View();
+        }
+        //用户注册信息显示
+        [Area("Admin")]
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
