a6930c5119fd52e5dafcb79ac9165ba981a252a6
872833248@qq.com <872833248@qq.com>
2021-03-05 09:32:07 +0800

    投诉举报，黑名单前台

---
+++ WSTWeb.MVC/Areas/Admin/Controllers/RecruitController.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+
+namespace WSTWeb.MVC.Areas.Admin.Controllers
+{
+    public class RecruitController : Controller
+    {
+        //显示
+        [Area("Admin")]
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
