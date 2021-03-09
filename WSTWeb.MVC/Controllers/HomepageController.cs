57ec08fb3575eeadb9b01077a4638ff2f6be1b4a
Yang <1963920080@qq.com>
2021-03-09 09:52:33 +0800

    首页页面

--- WSTWeb.MVC/Controllers/HomepageController.cs
+++ WSTWeb.MVC/Controllers/HomepageController.cs
@@ -12,5 +12,35 @@ namespace WSTWeb.MVC.Controllers
         {
             return View();
         }
+        //文章页面
+        public ActionResult ArticlePage()
+        {
+            return View();
+        }
+        //要闻资讯
+        public ActionResult ImpNews()
+        {
+            return View();
+        }
+        //成功的人
+        public ActionResult Succeed()
+        {
+            return View();
+        }
+        //规矩
+        public ActionResult Rule()
+        {
+            return View();
+        }
+        //廉洁
+        public ActionResult Honest()
+        {
+            return View();
+        }
+        //稳定
+        public ActionResult Steady()
+        {
+            return View();
+        }
     }
 }
