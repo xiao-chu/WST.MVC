f1e534cf6953c1a9f41064780fb50d627f64b341
sxy-zzj <79304949+sxy-zzj@users.noreply.github.com>
2021-03-09 09:50:47 +0800

    111

--- WSTWeb.MVC/Controllers/HomepageController.cs
+++ WSTWeb.MVC/Controllers/HomepageController.cs
@@ -8,7 +8,23 @@ namespace WSTWeb.MVC.Controllers
 {
     public class HomepageController : Controller
     {
+        //首页
         public IActionResult Index()
+        {
+            return View();
+        }
+        //关于我们
+        public IActionResult AboutUS()
+        {
+            return View();
+        }
+        //行为准则
+        public IActionResult AboutBehavior()
+        {
+            return View();
+        }
+        //行业/服务
+        public IActionResult Service2()
         {
             return View();
         }
